7ecf554b302999dd610af3caa263bb728350f594
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-08-12 14:10:23 -0400

    Datapath updates
    updated the datapath to operate like a pipelined CPU

--- source/datapath.v
+++ source/datapath.v
@@ -6,103 +6,255 @@
 
 // -------------------------------- //
 //	By: Bryce Keen	
-//	Created: 12/04/2022
+//	Created: 08/11/2023
 // -------------------------------- //
-//	Last Modified: 01/16/2022
+//	Last Modified: 08/12/2023
 
 // Change Log:	NA
 
-module datapath(clk, reset, regWE, rs1sel, 
-                rs2sel, regsel, PCsel, ImmSel, 
-                ALUControl,  Instr,  dmemData, 
-                pc, dmemrs2, ALUout);
+module datapath(
+  clk, reset,
+  reg_WE,
+  rs1_SEL, rs2_SEL,
+  pc_SEL, reg_SEL,
+  imm_SEL,
+  ALU_SEL,
+  Instr,
+  memDataRD,
+  pc,
+  memDataWD, memAdrs);
 
-    input clk, reset;
+  input             clk, reset;
+  input             reg_WE;
+  input             rs1_SEL, rs2_SEL;
+  input [1:0]       pc_SEL, reg_SEL;
+  input [2:0]       imm_SEL;
+  input [3:0]       ALU_SEL;
+  input [31:0]      Instr;
+  input [31:0]      memDataRD;
 
-    input regWE;
-    input rs1sel, rs2sel;
-    input [1:0] regsel, PCsel;
-    input [2:0] ImmSel;
-    input [3:0] ALUControl;
-    input [31:0] Instr;
-    input [31:0] dmemData;
+  output [31:0]     pc;
+  output [31:0]     memDataWD, memAdrs;
 
-    output [31:0]   pc, dmemrs2, ALUout; 
 
-    wire [31:0]     PC_now, PC_next;
-    wire [31:0]     rdout1, rdout2, wrs3;
-    wire [31:0]     ExtImm, muxrs1, muxrs2;
-    wire [31:0]     ALUResults;
-    wire [31:0]     pcplus4, pcPlusImm;
 
-    regfile regFILE (
-        .rs1(Instr[19:15]),
-        .rs2(Instr[24:20]),
-        .wrs3(wrs3),
-        .rd(Instr[11:7]),
-        .we(regWE),
-        .clk(clk),
-        .reset(reset),
-        .rdout1(rdout1),
-        .rdout2(rdout2));
-    assign dmemrs2 = rdout2;
 
-    flopr #(.WIDTH(32)) pcREG (
-        .d(PC_now), 
-        .q(PC_next), 
-        .clk(clk), 
-        .reset(reset));
-    assign pc = PC_next;
 
-    extend extendImm(
-        .Instr(Instr[31:7]), 
-        .ImmSrc(ImmSel), 
-        .ExtImm(ExtImm));
+  // ----------------------------- //
+  // Fetch
+  // ----------------------------- //
+  
+  wire [31:0]     pc_plus4_F, pc_F, instr_F;
+  wire [31:0]     pc_now, pc_next;
 
-    mux2 #(.WIDTH(32)) MUXrs1 (
-        .a(rdout1), 
-        .b(PC_next), 
-        .sel(rs1sel), 
-        .y(muxrs1));
+  mux2 #(.WIDTH(32)) MUX_pc_0 (
+    .a(pc_plus4_F), 
+    .b(pc_jump), 
+    .sel(pc_SEL[0]), 
+    .y(pc_now));
 
-    mux2 #(.WIDTH(32)) MUXrs2 (
-        .a(rdout2), 
-        .b(ExtImm), 
-        .sel(rs2sel), 
-        .y(muxrs2));
+  flopr #(.WIDTH(32)) REG_pc (
+      .d(pc_now), 
+      .q(pc_next), 
+      .clk(clk), 
+      .reset(reset));
 
-    alu32 alu (
-        .a(muxrs1), 
-        .b(muxrs2), 
-        .ALUControl(ALUControl), 
-        .result(ALUResults));
-    assign ALUout = ALUResults;
+  adder #(.WIDTH(32)) plus4 (
+    .a(4), 
+    .b(pc_next), 
+    .y(pc_plus4_F));
 
-    adder #(.WIDTH(32)) plus4 (
-        .a(4), 
-        .b(PC_next), 
-        .y(pcplus4));
+  assign instr_F = Instr;                   // Instruction input from memory
+  assign pc_F = pc_next;
+  assign pc = pc_next;                      // PC for Instruction memory
 
-    mux4 #(.WIDTH(32)) regmux (
-        .a(dmemData),
-        .b(ALUResults),
-        .c(ExtImm),
-        .d(pcplus4),
-        .sel(regsel), 
-        .y(wrs3));
 
-    mux4 #(.WIDTH(32)) pcmux (
-        .a(pcplus4),
-        .b(ALUResults),
-        .c(pcPlusImm),
-        .d(32'hXXXXXXXX),
-        .sel(PCsel), 
-        .y(PC_now));
 
-    adder #(.WIDTH(32)) adderImm (
-        .a(PC_next), 
-        .b(ExtImm), 
-        .y(pcPlusImm));
+
+
+
+
+  // ----------------------------- //
+  // Decode
+  // ----------------------------- //
+
+  reg [31:0]      pc_plus4_D = 0, pc_D = 0, instr_D = 0;
+  
+  wire            invrt_clk;
+  wire [31:0]     rdout1_D, rdout2_D, wrs3;
+  wire [31:0]     ExtImm_D
+
+
+  // REG_decode
+  always @(posedge clk, posedge reset) begin
+      if (reset) begin
+        pc_plus4_D = 0;
+        pc_D = 0;
+        instr_D = 0;
+      end
+      else begin
+        pc_plus4_D = pc_plus4_F;
+        pc_D = pc_F;
+        instr_D = instr_F;
+      end
+  end
+
+  assign invrt_clk = ~clk;
+
+  regfile regFILE (
+      .rs1(instr_D[19:15]),
+      .rs2(instr_D[24:20]),
+      .wrs3(wrs3),
+      .rd(instr_D[11:7]),
+      .we(reg_WE),
+      .clk(invrt_clk),
+      .reset(reset),
+      .rdout1(rdout1_D),
+      .rdout2(rdout2_D));
+
+  extend extendImm(
+      .Instr(instr_D[31:7]), 
+      .ImmSrc(imm_SEL), 
+      .ExtImm(ExtImm_D));
+  
+
+
+
+
+
+  // ----------------------------- //
+  // Execute
+  // ----------------------------- //
+  
+  reg [31:0]      pc_plus4_E = 0, pc_E = 0, ExtImm_E = 0;
+  reg [31:0]      rdout1_E = 0, rdout2_E = 0;
+
+  wire [31:0]     muxrs1, muxrs2;
+  wire [31:0]     ALUResults_E;
+  wire [31:0]     pcPlusImm_E;
+
+  // REG_execute
+  always @(posedge clk, posedge reset) begin
+    if (reset) begin
+      pc_E = 0;
+      pc_plus4_E = 0;
+      ExtImm_E = 0;
+      rdout1_E = 0;
+      rdout2_E = 0;
+    end
+    else begin
+      pc_E = pc_D;
+      pc_plus4_E = pc_plus4_D;
+      ExtImm_E = ExtImm_D;
+      rdout1_E = rdout1_D;
+      rdout2_E = rdout2_D;
+    end
+  end
+
+  mux2 #(.WIDTH(32)) MUX_rs1 (
+      .a(rdout1_E), 
+      .b(pc_E), 
+      .sel(rs1_SEL), 
+      .y(muxrs1));
+
+  mux2 #(.WIDTH(32)) MUX_rs2 (
+      .a(rdout2_E), 
+      .b(ExtImm_E), 
+      .sel(rs2_SEL), 
+      .y(muxrs2));
+
+  alu32 ALU (
+      .a(muxrs1), 
+      .b(muxrs2), 
+      .ALUControl(ALU_SEL), 
+      .result(ALUResults_E));
+
+  adder #(.WIDTH(32)) ADDER_Imm (
+      .a(pc_E), 
+      .b(ExtImm_E), 
+      .y(pcPlusImm_E));
+
+
+
+
+
+
+  // ----------------------------- //
+  // Memory
+  // ----------------------------- //
+
+  reg [31:0]      pc_plus4_M = 0, pcPlusImm_M = 0, ExtImm_M = 0;
+  reg [31:0]      rdout2_M = 0, ALUResults_M = 0;
+
+  wire [31:0]     pc_jump;
+  wire [31:0]     memData_M;
+
+
+  // REG_memory
+  always @(posedge clk, posedge reset) begin
+    if (reset) begin
+      pc_plus4_M = 0;
+      pcPlusImm_M = 0;
+      rdout2_M = 0;
+      ExtImm_M = 0;
+      ALUResults_M = 0;
+    end
+    else begin
+      ExtImm_M = ExtImm_E;
+      pc_plus4_M = pc_plus4_E;
+      pcPlusImm_M = pcPlusImm_E;
+      rdout2_M = rdout2_E;
+      ALUResults_M = ALUResults_E;
+    end
+  end
+
+  assign memAdrs = ALUResults_M;
+
+  mux2 #(.WIDTH(32)) MUX_pc_1 (
+      .a(ALUResults_M),
+      .b(pcPlusImm_M),
+      .sel(pc_SEL[1]), 
+      .y(pc_jump));
+
+  assign memData_M = memDataRD;
+
+
+
+
+
+
+  // ----------------------------- //
+  // Write Back
+  // ----------------------------- //
+
+  reg [31:0]      pc_plus4_WB = 0, memData_WB = 0, ALUResults_WB = 0;
+  reg [31:0]      ExtImm_WB = 0;
+
+  // REG_writeback
+  always @(posedge clk, posedge reset) begin
+    if (reset) begin
+      pc_plus4_WB = 0;
+      memData_WB = 0;
+      ALUResults_WB = 0;
+      ExtImm_WB = 0;
+    end
+    else begin
+      pc_plus4_WB = pc_plus4_M;
+      memData_WB = memData_M;
+      ALUResults_WB = ALUResults_M;
+      ExtImm_WB = ExtImm_M;
+    end
+  end
+
+
+  mux4 #(.WIDTH(32)) MUX_regfile (
+      .a(memData_WB),
+      .b(ALUResults_WB),
+      .c(ExtImm_WB),
+      .d(pc_plus4_WB),
+      .sel(reg_SEL), 
+      .y(wrs3));
+
 
 endmodule
 
