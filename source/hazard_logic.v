a48f7f064b290f038a190940edd9cdda8c2e2ea2
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-08-15 22:23:08 -0400

    Update hazard_logic.v

--- source/hazard_logic.v
+++ source/hazard_logic.v
@@ -21,17 +21,31 @@
 //  0 -> none
 //  1 -> rs3
 
-module hazard_logic(clk, reset, reg_WE, reg_RD, rs1, rs2, rs3, rs3_WB,
+module hazard_logic(clk, reset, reg_WE, reg_RD, rs1, rs2, rs3,
                     flush_F, flush_D, flush_E, flush_M, flush_WB,
                     stall_F, stall_D, stall_E, stall_M, stall_WB);
 
   input           clk, reset;
   input           reg_WE;
   input [1:0]     reg_RD;
-  input [4:0]     rs1, rs2, rs3, rs3_WB;
-  output reg      flush_F = 0, flush_D = 0, flush_E = 0, flush_M = 0, flush_WB = 0;
-  output reg      stall_F = 0, stall_D = 0, stall_E = 0, stall_M = 0, stall_WB = 0;
+  input [4:0]     rs1, rs2, rs3;
+  output wire     flush_F, flush_D, flush_E, flush_M, flush_WB;
+  output wire     stall_F, stall_D, stall_E, stall_M, stall_WB;
 
+
+  reg             flush_F_n = 0, flush_D_n = 0, flush_E_n = 0, flush_M_n = 0, flush_WB_n = 0;
+  reg             stall_F_n = 0, stall_D_n = 0, stall_E_n = 0, stall_M_n = 0, stall_WB_n = 0;
+
+  assign flush_F = flush_F_n;
+  assign flush_D = flush_D_n;
+  assign flush_E = flush_E_n;
+  assign flush_M = flush_M_n;
+  assign flush_WB = flush_WB_n;
+  assign stall_F = stall_F_n;
+  assign stall_D = stall_D_n;
+  assign stall_E = stall_E_n;
+  assign stall_M = stall_M_n;
+  assign stall_WB = stall_WB_n;
 
   // ----------------------------- //
   // Decode
@@ -73,15 +87,15 @@ module hazard_logic(clk, reset, reg_WE, reg_RD, rs1, rs2, rs3, rs3_WB,
   always @(*) begin
     if (rd_wr_collision) begin
       stall <= 1'b1;
-      stall_F <= 1'b1;
-      stall_D <= 1'b1; 
-      stall_E <= 1'b1; 
+      stall_F_n <= 1'b1;
+      stall_D_n <= 1'b1; 
+      stall_E_n <= 1'b1; 
     end
     else begin
       stall <= 1'b0;
-      stall_F <= 1'b0;
-      stall_D <= 1'b0; 
-      stall_E <= 1'b0; 
+      stall_F_n <= 1'b0;
+      stall_D_n <= 1'b0; 
+      stall_E_n <= 1'b0; 
     end
   end
 
@@ -91,76 +105,76 @@ module hazard_logic(clk, reset, reg_WE, reg_RD, rs1, rs2, rs3, rs3_WB,
 
   always @(posedge clk) begin
     if (stall) begin
-      flush_E <= 1'b1;
+      flush_E_n <= 1'b1;
     end
     else begin
-      flush_E <= 1'b0;
+      flush_E_n <= 1'b0;
     end
   end
 
-  // reg             reg_WE_E = 0;
-  // reg [4:0]       rs3_E = 0;
+  reg [4:0]       rs3_E = 0;
+  reg             reg_WE_E = 0;
 
-  // wire            en_E, reset_E;
-  // assign en_E = ~stall_E;
-  // assign reset_E = reset | flush_E;
+  wire            en_E, reset_E;
+  assign en_E = ~stall_E_n;
+  assign reset_E = reset | flush_E_n;
 
-  // // REG_execute
-  // always @(posedge clk, posedge reset_E) begin
-  //   if (reset_E) begin
-  //     rs3_E <= 0;
-  //     reg_WE_E <= 0;
-  //   end
-  //   else if (en_E) begin
-  //     rs3_E <= rs3;
-  //     reg_WE_E <= reg_WE;
-  //   end
-  // end
+  // REG_execute
+  always @(posedge clk, posedge reset_E) begin
+    if (reset_E) begin
+      rs3_E <= 0;
+      reg_WE_E <= 0;
+    end
+    else if (en_E) begin
+      rs3_E <= rs3;
+      reg_WE_E <= reg_WE;
+    end
+  end
 
   // ----------------------------- //
   // Memory
   // ----------------------------- //
 
-  // reg             reg_WE_M = 0;
-  // reg [4:0]       rs3_M = 0;
+  reg             reg_WE_M = 0;
+  reg [4:0]       rs3_M = 0;
 
-  // wire            en_M, reset_M;
-  // assign en_M = ~stall_M;
-  // assign reset_M = reset | flush_M;
+  wire            en_M, reset_M;
+  assign en_M = ~stall_M_n;
+  assign reset_M = reset | flush_M_n;
 
-  // // REG_memory
-  // always @(posedge clk, posedge reset_M) begin
-  //   if (reset_M) begin
-  //     rs3_M <= 0;
-  //     reg_WE_M <= 0;
-  //   end
-  //   else if (en_M) begin
-  //     rs3_M <= rs3_E;
-  //     reg_WE_M <= reg_WE_E;
-  //   end
-  // end
+  // REG_memory
+  always @(posedge clk, posedge reset_M) begin
+    if (reset_M) begin
+      rs3_M <= 0;
+      reg_WE_M <= 0;
+    end
+    else if (en_M) begin
+      rs3_M <= rs3_E;
+      reg_WE_M <= reg_WE_E;
+    end
+  end
 
   // ----------------------------- //
   // Write Back
   // ----------------------------- //
 
-  // reg             reg_WE_WB = 0;
-  // reg [4:0]       rs3_WB = 0;
+  reg             reg_WE_WB = 0;
+  reg [4:0]       rs3_WB = 0;
 
-  // wire            en_WB, reset_WB;
-  // assign en_WB = ~stall_WB;
-  // assign reset_WB = reset | flush_WB;
+  wire            en_WB, reset_WB;
+  assign en_WB = ~stall_WB_n;
+  assign reset_WB = reset | flush_WB_n;
 
-  // // REG_writeback
-  // always @(posedge clk, posedge reset_WB) begin
-  //   if (reset_WB) begin
-  //     reg_WE_WB <= 0;
-  //     rs3_WB <= 0;
-  //   end
-  //   else if (en_WB) begin
-  //     reg_WE_WB <= reg_WE_M;
-  //     rs3_WB <= rs3_M;
-  //   end
-  // end
+  // REG_writeback
+  always @(posedge clk, posedge reset_WB) begin
+    if (reset_WB) begin
+      reg_WE_WB <= 0;
+      rs3_WB <= 0;
+    end
+    else if (en_WB) begin
+      reg_WE_WB <= reg_WE_M;
+      rs3_WB <= rs3_M;
+    end
+  end
 
 endmodule
