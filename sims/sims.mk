5cdb2840ef11707811f4b71366789cd3c66d8649
Bryce Keen <bmkeen@liberty.edu>
2024-03-26 16:34:21 -0400

    Comments

--- sims/sims.mk
+++ sims/sims.mk
@@ -1,15 +1,18 @@
 
-# FOLDERS := $(wildcard *)
+# Get all dirs in 
 SIMS_FOLDERS := $(wildcard $(PROJECT_ROOT)/sims/*/)
 
-# Checks for tb.mk
-VALID_SIMS_FOLDERS :=
-$(foreach folder,$(SIMS_FOLDERS),$(if $(wildcard $(folder)/tb.mk),$(eval VALID_SIMS_FOLDERS += $(folder))))
 
 define sourcing_mk 
 include $(1)
 endef
 
+# ------------------------------------------------------------ #
+# Checks for tb.mk
+VALID_SIMS_FOLDERS :=
+$(foreach folder,$(SIMS_FOLDERS),$(if $(wildcard $(folder)/tb.mk),$(eval VALID_SIMS_FOLDERS += $(folder))))
+
+# Build Process for compiling testbenches
 define gen_build_target
 BUILD_DIR := $(1)
 
@@ -20,25 +23,15 @@ build-$(BUILD_NAME):
 	iverilog -o $(BUILD_NAME).out -DVCD_DUMP=1 $(TB_SOURCE) $(TB_INCLUDE) && \
 	vvp $(BUILD_NAME).out
 endef
+# ------------------------------------------------------------ #
 
+# Make Process
 $(foreach x, $(VALID_SIMS_FOLDERS), \
 	$(eval $(call sourcing_mk, $(x)tb.mk)) \
 	$(eval $(call gen_build_target, $(x))) \
 )
 
-# SIMS_FOLDERS_EDIT := $(patsubst %/,%,$(VALID_SIMS_FOLDERS))																															# Remove trailing '/'
-# Get the name of the testbench root folder
-# TESTBENCHES_BUILD := $(foreach folder,$(SIMS_FOLDERS_EDIT), $(notdir $(basename $(folder))))
-
-TARGET = 
-
-launch:
-	gtkwave $(TARGET)/$(TARGET)_tb.vcd
-
-gtkw:
-	gtkwave $(TARGET)/$(TARGET)_tb.gtkw
-
-# rm -f $(TARGET)/$(TARGET)_tb.out
+# Clean build
 clean:
 	$(foreach x,$(SIMS_FOLDERS), \
 		rm -f $(x)*_tb.vcd && \
