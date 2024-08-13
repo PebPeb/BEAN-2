81a06df9c8c7d44ef4b8784de41b19757ec707e7
PebPeb <67885698+PebPeb@users.noreply.github.com>
2024-08-12 23:04:34 -0400

    Adding sim make

--- sims/sims.mk
+++ sims/sims.mk
@@ -2,14 +2,15 @@
 # Get all dirs in 
 SIMS_FOLDERS := $(wildcard $(PROJECT_ROOT)/sims/*/)
 
-define sourcing_mk 
-include $(1)
-endef
-
-# ------------------------------------------------------------ #
 # Checks for tb.mk
 VALID_SIMS_FOLDERS :=
 $(foreach folder,$(SIMS_FOLDERS),$(if $(wildcard $(folder)/tb.mk),$(eval VALID_SIMS_FOLDERS += $(folder))))
+
+# ------------------------------------------------------------ #
+# Source mk files
+define sourcing_mk 
+include $(1)
+endef
 
 # Build Process for compiling testbenches
 define gen_build_target
@@ -21,17 +22,30 @@ build-$(BUILD_NAME):
 	iverilog -o $(BUILD_NAME).out -DVCD_DUMP=1 $(TB_SOURCE) $(TB_INCLUDE) && \
 	vvp $(BUILD_NAME).out
 endef
+
+define gtkwave_sim_target 
+BUILD_DIR_$(BUILD_NAME) := $(1)
+VCD_FILE := $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.vcd)
+
+ifneq ($(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.vcd)),)
+.PHONY: gtk-$(BUILD_NAME)
+gtk-$(BUILD_NAME):
+	@echo $(VCD_FILE)
+endif
+endef
+
 # ------------------------------------------------------------ #
 
 # Make Process
 $(foreach x, $(VALID_SIMS_FOLDERS), \
 	$(eval $(call sourcing_mk, $(x)tb.mk)) \
 	$(eval $(call gen_build_target, $(x))) \
+	$(eval $(call gtkwave_sim_target, $(x))) \
 )
 
 # Clean build
 clean:
-	$(foreach x,$(SIMS_FOLDERS), \
+	$(foreach x,$(VALID_SIMS_FOLDERS), \
 		rm -f $(x)*_tb.vcd && \
 		rm -f $(x)*.out \
 	)
