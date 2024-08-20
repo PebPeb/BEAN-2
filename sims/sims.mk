dbf8a7a938873a6a3f3e233e54424eaa4375c15b
PebPeb <67885698+PebPeb@users.noreply.github.com>
2024-08-19 23:53:48 -0400

    Added .gtkw discovery

--- sims/sims.mk
+++ sims/sims.mk
@@ -27,11 +27,19 @@ define gtkwave_sim_target
 BUILD_DIR_$(BUILD_NAME) := $(1)
 
 ifneq ($(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.vcd)),)
-.PHONY: gtk-$(BUILD_NAME)
-gtk-$(BUILD_NAME):
+.PHONY: gtk-$(BUILD_NAME)-vcd
+gtk-$(BUILD_NAME)-vcd:
 	gtkwave $(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.vcd))
+
+ifneq ($(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.gtkw)),)
+.PHONY: gtk-$(BUILD_NAME)-$(notdir $(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.gtkw)))
+gtk-$(BUILD_NAME)-$(notdir $(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.gtkw))):
+	gtkwave $(strip $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.gtkw))
+endif
+
 endif
 endef
+
 
 # @echo $(notdir $(wildcard $(BUILD_DIR_$(BUILD_NAME))*.vcd))
 # ------------------------------------------------------------ #
